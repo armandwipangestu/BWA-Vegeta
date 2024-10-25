e815e40ad547794ceacdab0ca8d452b18bc77e6c
Arman <armandwi.pangestu7@gmail.com>
2024-10-25 14:54:03 +0700

    Sign In Response Helper

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,19 +1,14 @@
+import Response from "@/lib/api.response";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, params: Params) {
   const id = params.params.id;
-  return NextResponse.json(
-    {
-      success: true,
-      message: `Get detail users by id ${id}`,
-      data: {
-        id,
-        name: "Arman",
-      },
+  return Response({
+    message: `Get detail users by id ${id}`,
+    data: {
+      id,
+      name: "Arman",
     },
-    {
-      status: 200,
-    }
-  );
+  });
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,39 +1,29 @@
+import Response from "@/lib/api.response";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(
-    {
-      success: true,
-      message: "Get all users",
-      data: [
-        {
-          id: 1,
-          name: "Arman",
-        },
-        {
-          id: 2,
-          name: "Dwi",
-        },
-      ],
-    },
-    {
-      status: 200,
-    }
-  );
-}
-
-export async function POST() {
-  return NextResponse.json(
-    {
-      success: true,
-      message: "New users created",
-      data: {
+  return Response({
+    message: "Get all users",
+    data: [
+      {
         id: 1,
         name: "Arman",
       },
+      {
+        id: 2,
+        name: "Dwi",
+      },
+    ],
+    status: 200,
+  });
+}
+
+export async function POST() {
+  return Response({
+    message: "New users created",
+    data: {
+      id: 1,
+      name: "Arman",
     },
-    {
-      status: 201,
-    }
-  );
+  });
 }
